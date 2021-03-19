3f156b6d56846bce23b411fb13f9d5b4b1d48185
Carlos Novaes <carlos.novaes@b2wdigital.com>
2021-03-19 10:09:35 -0300

    criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello World</h1>
     </div>
   );
